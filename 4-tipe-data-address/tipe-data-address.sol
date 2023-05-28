0ea3515203812974e47c555eb192c6bf55bf21ff
Muhamad Ali Nur Zaman <muhamadalinurzaman@gmail.com>
2023-05-28 08:36:52 +0700

    tipe data address

---
+++ 4-tipe-data-address/tipe-data-address.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract TipeDataAddress {
+    address public myAddress = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    address public defaultAddress;
+}
